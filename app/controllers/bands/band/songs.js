b3d10609c28cce26aba1419a2ca37cd22b801a51
Fabio Callegari <fabio.callegari@fabio.callegari-JFHY622JFX>
2025-04-16 15:14:50 -0700

    refactor services

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,6 @@
 import Controller from "@ember/controller";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
-import Song from "rarwe/models/song";
 import { service } from "@ember/service";
 
 export default class BandsBandSongsController extends Controller {
@@ -18,23 +17,7 @@ export default class BandsBandSongsController extends Controller {
   async updateRating(song, rating) {
     song.rating = rating;
 
-    let payload = {
-      data: {
-        id: song.id,
-        type: "songs",
-        attributes: {
-          rating,
-        },
-      },
-    };
-
-    await fetch(`/songs/${song.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/vnd.api+json",
-      },
-      body: JSON.stringify(payload),
-    });
+    this.catalog.update("song", song, { rating });
   }
 
   @action
@@ -44,38 +27,12 @@ export default class BandsBandSongsController extends Controller {
 
   @action
   async saveSong() {
-    let payload = {
-      data: {
-        type: "songs",
-        attributes: { title: this.title },
-        relationships: {
-          band: {
-            data: {
-              id: this.model.id,
-              type: "bands",
-            },
-          },
-        },
-      },
-    };
-    let response = await fetch("/songs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/vnd.api+json",
-      },
-      body: JSON.stringify(payload),
-    });
+    let song = await this.catalog.create(
+      "song",
+      { title: this.title },
+      { band: { data: { id: this.model.id, type: "bands" } } }
+    );
 
-    let json = await response.json();
-    let { id, attributes, relationships } = json.data;
-    let rels = {};
-
-    for (let relationshipName in relationships) {
-      rels[relationshipName] = relationships[relationshipName].links.related;
-    }
-
-    let song = new Song({ id, ...attributes }, rels);
-    this.catalog.add("song", song);
     this.model.songs = [...this.model.songs, song];
     this.title = "";
     this.showAddSong = true;
